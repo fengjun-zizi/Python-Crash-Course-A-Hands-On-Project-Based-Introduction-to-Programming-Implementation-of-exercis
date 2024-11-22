400e439dde05682a9444f0197a2fc6d517e94872
fengjun-zizi <feng2131545912@icloud.com>
2024-11-22 14:20:28 +0700

    Create practice_like_number.py

---
+++ practice_like_number.py
@@ -0,0 +1,7 @@
+like_number = {'number1':'6' , 'number2' : '66' , 'number3' : '666' , 'number4' : '9' , 'number5' : '999'}
+
+print(like_number['number1'])
+print(like_number['number2'])
+print(like_number['number3'])
+print(like_number['number4'])
+print(like_number['number5'])
