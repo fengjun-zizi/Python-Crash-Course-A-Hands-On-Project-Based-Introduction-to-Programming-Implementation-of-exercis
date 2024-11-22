e502e216f1a876c6a8928164303c696b5e393f61
fengjun-zizi <feng2131545912@icloud.com>
2024-11-22 17:56:36 +0700

    Update practice_6.6_Invsetigation.py

--- practice_6.6_Invsetigation.py
+++ practice_6.6_Invsetigation.py
@@ -1,1 +1,16 @@
+favorite_language = {
+    'jen' : 'python' ,
+    'sarah' : 'C++' , 
+    'edward' : 'rust' , 
+    'phil' : 'python' 
+}
 
+person = { 'jen' , 'steve' ,  'phoil' , 'fengjun' }
+
+
+print(f"These are the people who should be investigated : {person}")
+for persones in person :
+    if persones in favorite_language.keys() :
+        print(f"Thank you for your cooperation : {persones}")
+    else :
+        print(f"Please cooperate with us : {persones}")
