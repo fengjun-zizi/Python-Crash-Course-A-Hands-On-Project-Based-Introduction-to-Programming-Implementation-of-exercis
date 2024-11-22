59df3d0da1618dccf8e5b59b390b16dff128e839
fengjun-zizi <feng2131545912@icloud.com>
2024-11-22 14:39:42 +0700

    Create glossary.py

---
+++ glossary.py
@@ -0,0 +1,4 @@
+python_glossary = {'字典' : '是一系列建值位' , '变量' : '每个变量都指向一个值'  , '字符串' : '是一系列字符串' , '数' : '表示可视化数据、储存信息等' , '浮点数' : '带有小数点的数' }
+
+for term, definition in python_glossary.items():
+    print(f"{term}: {definition}")
