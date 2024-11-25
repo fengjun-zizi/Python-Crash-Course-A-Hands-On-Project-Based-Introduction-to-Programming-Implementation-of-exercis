62e45fcab63ac4b3025518a4361960ec28319891
fengjun-zizi <feng2131545912@icloud.com>
2024-11-25 19:04:14 +0700

    Add files via upload

---
+++ practice_7.10_The_dream_vacation_destination.py
@@ -0,0 +1,2 @@
+Dream_vacation_destination = input(" If you could visit one  place in the world , would you go !")
+print(f" I  want to go the {Dream_vacation_destination}")
---
+++ practice_7.8_sandwich_orders.py
@@ -0,0 +1,20 @@
+sandwich_orders = [
+    "Classic Club Delight",  # 经典俱乐部三明治
+    "Spicy Buffalo Crunch",  # 辣味水牛城酥脆三明治
+    "Mediterranean Bliss",   # 地中海风味三明治
+    "Veggie Garden Feast",   # 蔬菜花园盛宴三明治
+    "Smoky BBQ Pulled Pork", # 烟熏烧烤猪肉三明治
+    "Avocado Turkey Fusion", # 牛油果火鸡融合三明治
+    "Cheesy Tuna Melt",      # 芝士金枪鱼融化三明治
+    "Honey Mustard Chicken Supreme", # 蜂蜜芥末鸡肉至尊三明治
+    "Egg & Bacon Sunrise",   # 蛋培根日出三明治
+    "Italian Caprese Twist"  # 意式卡普雷塞风味三明治
+]
+
+finished_sandwiches = []
+
+while sandwich_orders :
+    finished = sandwich_orders.pop()
+
+    print(f"I made your sandwiches : {finished}")
+    finished_sandwiches.append(finished)
